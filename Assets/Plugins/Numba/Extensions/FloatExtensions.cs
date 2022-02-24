519f4ddde7a3f29514ac3736eba1a2a9f72e4d4d
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2022-02-24 16:52:15 +0300

    Remove NearlyEquals methods from float and double types

--- Assets/Plugins/Numba/Extensions/FloatExtensions.cs
+++ Assets/Plugins/Numba/Extensions/FloatExtensions.cs
@@ -9,17 +9,6 @@ namespace Extensions
 {
     public static class FloatExtensions
     {
-        public static bool NearlyEquals(this float value, float other, float epsilon = 0.0000001f)
-        {
-            if (value == other) // Simple check, also handles infinities.
-                return true;
-
-            value = Mathf.Abs(value);
-            other = Mathf.Abs(other);
-
-            return Mathf.Abs(value - other) < epsilon;
-        }
-
         public static float Remap(this float value, float min1, float max1, float min2, float max2)
         {
             return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
